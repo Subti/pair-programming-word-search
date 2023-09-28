8de29421cee27e624e8d20362f9a91b4003a6499
JP Saliba <jpsaliba@outlook.com>
2023-09-27 22:20:57 -0400

    My pair programming partner never responded :(

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,25 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+    const numCols = letters[0].length;
+    const numRows = letters.length;
 
-module.exports = wordSearch
+    for (let j = 0; j < numCols; j++) {
+        let col = '';
+        for (let i = 0; i < numRows; i++) {
+            col += letters[i][j];
+        }
+        if (col.includes(word)) {
+            return true;
+        }
+    }
+
+    for (let i = 0; i < numRows; i++) {
+        const row = letters[i].join('');
+        if (row.includes(word)) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
+module.exports = wordSearch;
